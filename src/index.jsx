5094dfdf10b55dde9359e6866fef119b2365f7f5
sambhav <sambhav20032004@gmail.com>
2026-04-20 19:13:27 +0530

    Removed stray backticks from config files

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './test.css';
 
 // Register Service Worker
 if ('serviceWorker' in navigator) {
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './test.css'
 
 // FORCE DARK MODE BY DEFAULT (This fixes the white background issue)
 document.documentElement.classList.add('dark');
