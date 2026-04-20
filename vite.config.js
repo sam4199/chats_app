f38a2a83513b0d640cdc7b1a2a65ff774eecbb3f
sambhav <sambhav20032004@gmail.com>
2026-04-20 09:45:07 +0530

    Removed stray backticks from config files

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
